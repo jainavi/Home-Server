96663638539c4689eb83af9d04b045ce3f9683c6
Avi Jain <jainavi1408@gmail.com>
2024-06-30 00:40:23 +0530

    Add the logger

---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import path from 'path';
+
+const logFilePath = path.join(__dirname, '../../logs/app.log');
+const errorFilePath = path.join(__dirname, '../../logs/error.log');
+
+const fileLogStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+const errorLogStream = fs.createWriteStream(errorFilePath, { flags: 'a' });
+
+const getCurrentDateTime = () => {
+  const now = new Date();
+  return `${now.toISOString().slice(0, 10)} ${now.toTimeString().slice(0, 10)}`;
+};
+
+const formatLogMessage = (type: string, message: string, payload?: any) => {
+  const formattedPayload = payload
+    ? `\nPayload: ${JSON.stringify(payload, null, 2)}`
+    : '';
+  return `${getCurrentDateTime()} | ${type} | ${message}${formatLogMessage}`;
+};
+
+const formatErrorMessage = (error: Error, payload?: any) => {
+  const stackTrace = error.stack ? `\nStack Trace: ${error.stack}` : '';
+  const formattedPayload = payload
+    ? `\nPayload: ${JSON.stringify(payload, null, 2)}`
+    : '';
+  return `${getCurrentDateTime()} | ERROR | ${
+    error.message
+  }${stackTrace}${formattedPayload}\n`;
+};
+
+const log = (message: string, payload?: any) => {
+  const formattedMessage = formatLogMessage('INFO', message, payload);
+
+  if (process.env.NODE_ENV === 'production') {
+    fileLogStream.write(formattedMessage);
+  } else if (process.env.NODE_ENV === 'development')
+    console.log(formattedMessage);
+};
+
+const error = (error: Error, payload?: any) => {
+  const formattedErrorMessage = formatErrorMessage(error, payload);
+
+  if (process.env.NODE_ENV === 'development')
+    console.error(formattedErrorMessage);
+  else if (process.env.NODE_ENV === 'production') {
+    errorLogStream.write(formattedErrorMessage);
+  }
+};
+
+export default { log, error };
